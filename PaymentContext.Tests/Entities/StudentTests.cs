98f92a35b738692b502b315e2637abe6df96eb4e
Patrick Patchelli <patrick.patchelli@hotmail.com>
2022-12-09 21:12:10 -0300

    create project rich domains

---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PaymentContext.Domain.Entities;
+using Xunit;
+
+namespace PaymentContext.Tests.Entities
+{
+    public class StudentTests
+    {
+        [Fact]
+        public void TestName()
+        {
+            var student = new Student("Patrick", "Patchelli", "10.900.199-6", "ppatchelli@gmail.com");
+        }
+    }
+}
